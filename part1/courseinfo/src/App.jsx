de7d7a1f3ef768fdea52c3be51a9912a3392ac89
Abhay <abhayn247@gmail.com>
2026-05-31 15:28:40 -0400

    completion of exercise1.1 implementing Header, Content and Total components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,6 +10,32 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+}
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -23,16 +49,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
