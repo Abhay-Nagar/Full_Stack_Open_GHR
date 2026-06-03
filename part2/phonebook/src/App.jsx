ffb01d511e7bcf111755aafcfcdc947801f74a94
Abhay <abhayn247@gmail.com>
2026-06-02 20:48:45 -0400

    phonebook completed to exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 const Number = ({person}) => {
 
 return(
@@ -13,15 +13,22 @@ const Filter = ({value, onChange}) => <div>filter shown with <input value={value
 const Persons = ({persons}) => <div>{persons.map(person => <Number key={person.name} person={person}/>)}</div>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() =>{
+
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then( response =>{
+
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+
+  },[])
+
 
   const handleNewName = (event) =>{
     
