18756380449052bb9c26921ea27762412654e45d
Abhay <abhayn247@gmail.com>
2026-06-03 21:19:48 -0400

    My finished info for countries app!

--- part2/data-for-countries/src/components/Countryinfo.jsx
+++ part2/data-for-countries/src/components/Countryinfo.jsx
@@ -1,10 +1,30 @@
 import Languages from './Languages'
 import Capitals from './Capitals'
+import countryService from '../services/countries'
+import {useState, useEffect} from 'react'
 
 const Countryinfo = ({country}) =>{
 
-    console.log('this is country info',country)
+    const [weather, setWeather] = useState(null)
+
+    console.log('this is country info',country.capitalInfo.latlng)
     const imageUrl = country.flags.png
+    
+
+useEffect(() => {
+    countryService.getWeather(country).then((response) =>{
+    setWeather(response)
+    })
+    console.log(weather)
+},[country])
+   
+
+if(!weather){
+    return
+}
+const icon = weather.weather[0].icon
+const iconUrl = `https://openweathermap.org/payload/api/media/file/${icon}.png`
+
 return(
     <div>
         <h1>{country.name.common}</h1>
@@ -14,6 +34,9 @@ return(
         <h2>Languages</h2>
         <Languages languages={country.languages}/>
         <img src={imageUrl} alt={`Flag of ${country.name.common}`}/>
+        <p>Temp: {weather.main.temp} Celsius</p>
+        <img src={iconUrl}/>
+        <p>Wind Speed: {weather.wind.speed} m/s</p>
     </div>
     
 )}
--- part2/data-for-countries/src/services/countries.js
+++ part2/data-for-countries/src/services/countries.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const baseURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const api_key = import.meta.env.VITE_SOME_KEY
+// variable api_key now has the value set in startup
 
 const getAll = () =>{
 
@@ -11,4 +13,17 @@ return request.then(response =>response.data)
     
 }
 
-export default {getAll}
+
+const getWeather = (country) => {
+
+  const [lat, lon] = country.capitalInfo.latlng
+
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`
+    )
+    .then(response => response.data)
+}
+
+export default {getAll, getWeather}
+
