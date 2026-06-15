571fe8805f50fa47990d3d0c921d0f42264d5fc1
Abhay <abhayn247@gmail.com>
2026-06-15 15:09:45 -0400

    completion of exc 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -9,6 +9,9 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -16,16 +19,35 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUser){
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      console.log('this is effect', user)
+      blogService.setToken(user.token)
+    }
+    
+  },[])
+
+
+ 
+  
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
-      const usert = await loginService.login({ username, password })
-      setUser(usert)
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user)) 
+      setUser(user)
       console.log('wtfdgdgd', user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
     } catch {
       setErrorMessage('wrong credentials')
+      setUsername('')
+      setPassword('')
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -33,7 +55,30 @@ const App = () => {
     
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+    blogService.setToken('')
+  }
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try{
+    const blog = await blogService.create({title, author, url})
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setBlogs(blogs.concat(blog))
+    console.log('this is blog',blog)
+    } catch (exception){
+      console.log(exception)
+    }
+  }
+
+
   const loginForm = () => (
+    <div>
+    <h1>login form</h1>
     <form onSubmit={handleLogin}>
       <div>
         <div>
@@ -51,16 +96,48 @@ const App = () => {
         <button type="submit">login</button>
       </div>
     </form>
+    </div>
+  )
+
+  const loggedIn = () => (
+    <div>
+    <h2>blogs</h2>
+      <div>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </div>
+      <form onSubmit={handleCreate}>
+      <div>
+        <div>
+        <label>
+          title
+          <input type="text" value={title} onChange={({target}) => setTitle(target.value)}/>
+        </label>
+        </div>
+        <div>
+        <label>
+          author
+          <input type="text" value={author} onChange={({target}) => setAuthor(target.value)}/>
+        </label>
+        </div>
+        <div>
+        <label>
+          url
+          <input type="text" value={url} onChange={({target}) => setUrl(target.value)}/>
+        </label>
+        </div>
+        <button type="submit">create</button>
+      </div>
+    </form>
+      
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+    </div>
   )
 
   return (
     <div>
       <h2>{errorMessage}</h2>
-      {loginForm()}
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && loginForm()}
+      {user && loggedIn()}
     </div>
   )
 }
--- part5/bloglist-frontend/src/services/login.js
+++ part5/bloglist-frontend/src/services/login.js
@@ -4,7 +4,8 @@ const baseUrl = '/api/login'
 
 const login = async (credentials) => {
 
-  const response  = axios.post(baseUrl, credentials)
+  const response  = await axios.post(baseUrl, credentials)
+  console.log('this is resposnesesses',response.data)
   return response.data
 
 }
